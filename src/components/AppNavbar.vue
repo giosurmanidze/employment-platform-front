0ebb98c70520f510e78040a5f3e740a0be20c857
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-02-24 18:04:27 +0400

    add

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -20,33 +20,9 @@
 
         <!-- input fields -->
         <div class="flex flex-col items-center justify-center mt-4 gap-1">
-          <div class="relative mb-3 w-[90%]">
-            <input
-              type="email"
-              class="peer m-0 block h-[58px] rounded-xl w-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label
-              for="floatingInput"
-              class="pointer-events-none text-sm font-medium absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-              >მომხმარებელის სახელი / ელ.ფოსტა</label
-            >
-          </div>
+          <text-field name="username" type="text" label="username" />
+          <text-field name="password" />
 
-          <div class="relative mb-3 w-[90%]">
-            <input
-              type="email"
-              class="peer m-0 block h-[58px] rounded-xl w-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label
-              for="floatingInput"
-              class="pointer-events-none text-sm font-medium absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-              >პაროლი</label
-            >
-          </div>
           <div
             class="flex w-[90%] font-medium text-[#8f9b9a] text-[0.775rem] underline cursor-pointer"
           >
@@ -74,6 +50,7 @@
 
 <script setup>
 import { ref } from "vue";
+import TextField from "@/components/TextField.vue";
 
 const showModal = ref(false);
 
