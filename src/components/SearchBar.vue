7b3a92e30164a8f26c55e4a18aa1a6803586da73
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-07 22:16:32 +0400

    style/make search bar responsive

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="w-full flex justify-center flex-col items-center">
-    <div class="w-[60rem]">
-      <h1 class="text-[2.5rem] text-primary">იპოვე სამსახური აქ!</h1>
+    <div class="sm:w-[80%] lg:w-[70%] sm:mt-28 md:mt-0">
+      <h1 class="sm:text-[2rem] md:text-[2.5rem] text-primary">იპოვე სამსახური აქ!</h1>
       <span class="text-main_text_color text-[1.063rem]"
         >ჩვენს პლატფორმაზე მარტივად შეძლებ პროფესიონალური CV-ს შედგენას, ვაკანსიების
         ადგილმდებარეობით/ფილტრებით მოძიებას და საიტიდან გაუსვლელად CV-ს გაგზავნას</span
       >
-      <form class="mt-8 flex w-full drop-shadow-md">
+      <form class="mt-8 flex w-full drop-shadow-md sm:flex-col md:flex-row">
         <div class="w-full">
           <label for="search_bar " class="absolute t-0 z-20 pl-5 pt-2">ვაკანსიების ძიება</label>
           <input
             id="search_bar"
             placeholder="მაგ:პროგრამისტი"
-            class="w-full relative h-20 outline-none opacity-50 pt-5 pl-5 rounded-tl-3xl rounded-bl-3xl border-r-2 border-gray-300"
+            class="w-full relative h-20 outline-none pt-5 pl-5 sm:rounded-tl-3xl sm:rounded-tr-3xl md:rounded-tr-none md:rounded-bl-3xl border-r-2 border-gray-300"
           />
         </div>
         <div class="w-full">
@@ -20,13 +20,14 @@
           <input
             id="search_bar"
             placeholder="მიუთითე სასურველი ლოკაცია"
-            class="w-full h-20 outline-none pl-5 opacity-50 pt-5 border-r-2 border-gray-300"
+            class="w-full h-20 outline-none pl-5 pt-5 border-r-2 border-gray-300"
           />
         </div>
         <button
-          class="h-20 bg-orange_red_color outline-none p-5 rounded-tr-3xl rounded-br-3xl flex justify-center items-center"
+          class="h-20 bg-orange_red_color outline-none p-5 md:rounded-tr-3xl sm:rounded-bl-3xl md:rounded-bl-none sm:rounded-br-3xl flex justify-center items-center"
         >
-          <img src="@/assets/images/search.png" class="z-50" width="60" height="60" />
+          <img src="@/assets/images/search.png" class="sm:hidden md:block" width="60" height="60" />
+          <img src="@/assets/images/search.png" class="sm:block md:hidden" width="40" height="40" />
         </button>
       </form>
     </div>
