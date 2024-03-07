07cb8ff9e1cc8be4a197a279b2f65439a6f5f48b
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-07 23:42:22 +0400

    feat/fill carousel content with vacancy data

--- src/components/PrimeCarousel.vue
+++ src/components/PrimeCarousel.vue
@@ -1,35 +1,21 @@
 <script setup>
-import Carousel from "primevue/carousel";
-import { ref } from "vue";
-
-const vacancies = ref([
-  {
-    name: "Product 1",
-    image: "image_1.avif",
-  },
-  {
-    name: "Product 1",
-    image: "image_1.avif",
-  },
-  {
-    name: "Product 1",
-    image: "image_1.avif",
-  },
-  {
-    name: "Product 1",
-    image: "image_1.avif",
-  },
-  {
-    name: "Product 1",
-    image: "image_1.avif",
-  },
-]);
+import { useVacancyStore } from '@/stores/vacancy/useVacancyStore'
+import { storeToRefs } from 'pinia'
+import Carousel from 'primevue/carousel'
+import { onMounted } from 'vue'
 
 const responsiveOptions = [
-  { breakpoint: "1024px", numVisible: 4, numScroll: 4 },
-  { breakpoint: "768px", numVisible: 2, numScroll: 2 },
-  { breakpoint: "560px", numVisible: 1, numScroll: 1 },
-];
+  { breakpoint: '1024px', numVisible: 4, numScroll: 4 },
+  { breakpoint: '768px', numVisible: 2, numScroll: 2 },
+  { breakpoint: '560px', numVisible: 1, numScroll: 1 }
+]
+
+const { getVacancies } = useVacancyStore()
+const { vacancies } = storeToRefs(useVacancyStore())
+
+onMounted(() => {
+  getVacancies()
+})
 </script>
 
 <template>
@@ -41,23 +27,21 @@ const responsiveOptions = [
   >
     <template #item="slotProps">
       <div class="border-1 surface-border border-round m-2b p-3 h-full">
-        <div
-          class="mb-3 bg-[#649c9e] h-[15rem] rounded-2xl p-3 flex flex-col gap-5 text-white"
-        >
+        <div class="mb-3 bg-primary h-[17rem] rounded-2xl p-3 flex flex-col gap-5 text-white">
           <div class="relative mx-auto w-full flex justify-between items-center">
             <img
-              src="../assets/images/image_1.avif"
-              alt="Image"
+              :src="`http://localhost:8000/storage/${slotProps.data.creator.image}`"
+              :alt="slotProps.data.image"
               class="w-[5rem] h-[5rem] rounded-full"
             />
-            <div>{{ slotProps.data.name }}</div>
+            <span class="text-sm">7მარ-6აპრ</span>
           </div>
           <div class="flex flex-col gap-2">
             <div>
-              <p class="text-sm">კომპანიის სახელი</p>
-              <p>ვაკანსიის დასახელება</p>
+              <p class="text-sm">{{ slotProps.data.creator.name }}</p>
+              <p>{{ slotProps.data.title }}</p>
             </div>
-            <span>მისამართი</span>
+            <span>{{ slotProps.data.creator.address }}</span>
           </div>
         </div>
       </div>
