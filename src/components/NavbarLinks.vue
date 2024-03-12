190f95906119cd5393d6ffd2488519bc2ee8955b
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-12 23:57:09 +0400

    fix/change a tag with router link

--- src/components/NavbarLinks.vue
+++ src/components/NavbarLinks.vue
@@ -1,34 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
   <ul
     class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
   >
     <li>
-      <a
-        href="#"
+      <RouterLink
+        to=""
         class="block py-2 px-3 md:p-0 text-gray-900 bg-orange_red_color rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-        aria-current="page"
-        >მთავარი</a
+        >მთავარი</RouterLink
       >
     </li>
     <li>
-      <a
-        href="#"
+      <RouterLink
+        to=""
         class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-        >ვაკანსიები</a
+        >ვაკანსიები</RouterLink
       >
     </li>
     <li>
-      <a
-        href="#"
+      <RouterLink
+        to=""
         class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-        >განათლება</a
+        >განათლება</RouterLink
       >
     </li>
     <li>
-      <a
-        href="#"
+      <RouterLink
+        to=""
         class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-        >ფასები</a
+        >ფასები</RouterLink
       >
     </li>
   </ul>
